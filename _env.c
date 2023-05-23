d6c76876bd056fcd9a45a0edad9f908e7a6fafb7
NIYIBIZI Elysée <125453474+elyse502@users.noreply.github.com>
2023-05-23 21:33:16 +0200

    Env.c

--- _env.c
+++ _env.c
@@ -8,37 +8,37 @@
  */
 int _setenv(char *var_name, char *var_value)
 {
-	int i = 0;
-	size_t name_len;
-	char *var_new;
+        int i = 0;
+        size_t name_len;
+        char *var_new;
 
-	name_len = _strlen(var_name);
-	i = 0;
-	/*updating an existing variable*/
-	while (environ[i])
-	{
-		if (strncmp(environ[i], var_name, name_len) == 0)
-		{
-			var_new = var_build(var_name, var_value);
-			/*Not sure but wanted to clear its mem b4 writing*/
-			environ[i] = NULL;
-			environ[i] = _strdup(var_new);
-			free(environ[i]);
-			environ[i] = _strdup(var_new);
-			free(var_new);
-			return (0);
-		}
-		i++;
-	}
-	/*adding a variable that never existed before*/
-	var_new = var_build(var_name, var_value);
-	free(environ[i]);
-	environ[i] = _strdup(var_new);
-	i++;
-	environ[i] = NULL;
-	var_new = NULL;
+        name_len = _strlen(var_name);
+        i = 0;
+        /*updating an existing variable*/
+        while (environ[i])
+        {
+                if (strncmp(environ[i], var_name, name_len) == 0)
+                {
+                        var_new = var_build(var_name, var_value);
+                        /*Not sure but wanted to clear its mem b4 writing*/
+                        environ[i] = NULL;
+                        environ[i] = _strdup(var_new);
+                        free(environ[i]);
+                        environ[i] = _strdup(var_new);
+                        free(var_new);
+                        return (0);
+                }
+                i++;
+        }
+        /*adding a variable that never existed before*/
+        var_new = var_build(var_name, var_value);
+        free(environ[i]);
+        environ[i] = _strdup(var_new);
+        i++;
+        environ[i] = NULL;
+        var_new = NULL;
 
-	return (0);
+        return (0);
 }
 /**
  * var_build - Builds an environment variable from its name and value.
@@ -48,23 +48,23 @@ int _setenv(char *var_name, char *var_value)
  */
 char *var_build(char *var_name, char *var_value)
 {
-	char *new_var;
-	size_t var_len;
+        char *new_var;
+        size_t var_len;
 
-	var_len = _strlen(var_name) + _strlen(var_value) + 2;
-	new_var = malloc(sizeof(char) * var_len);
-	if (new_var == NULL)
-	{
-		perror("Error: Insufficient memory\n");
-		return (NULL);
-	}
-	_memset(new_var, 0, var_len);
-	/*Data in the form: var_name=var_value*/
-	new_var = _strcat(new_var, var_name);
-	new_var = _strcat(new_var, "=");
-	new_var = _strcat(new_var, var_value);
+        var_len = _strlen(var_name) + _strlen(var_value) + 2;
+        new_var = malloc(sizeof(char) * var_len);
+        if (new_var == NULL)
+        {
+                perror("Error: Insufficient memory\n");
+                return (NULL);
+        }
+        memset(new_var, 0, var_len);
+        /*Data in the form: var_name=var_value*/
+        new_var = _strcat(new_var, var_name);
+        new_var = _strcat(new_var, "=");
+        new_var = _strcat(new_var, var_value);
 
-	return (new_var);
+        return (new_var);
 }
 /**
  * _unsetenv - removes an environment variable.
@@ -73,27 +73,27 @@ char *var_build(char *var_name, char *var_value)
  */
 int _unsetenv(char *var_name)
 {
-	int i = 0;
-	char **env_temp;
-	size_t name_len;
+        int i = 0;
+        char **env_temp;
+        size_t name_len;
 
-	name_len = _strlen(var_name);
-	while (environ[i])
-	{
-		if (strncmp(environ[i], var_name, name_len) == 0)
-		{
-			if (environ[i] != NULL)
-			{
-				env_temp = environ;
-				free(env_temp[i]);
-				do {
-					env_temp[i] = env_temp[i + 1];
-					i++;
-				} while (env_temp[i + 1] != NULL);
-				env_temp[i] = NULL;
-			}
-		}
-		i++;
-	}
-	return (0);
+        name_len = _strlen(var_name);
+        while (environ[i])
+        {
+                if (strncmp(environ[i], var_name, name_len) == 0)
+                {
+                        if (environ[i] != NULL)
+                        {
+                                env_temp = environ;
+                                free(env_temp[i]);
+                                do {
+                                        env_temp[i] = env_temp[i + 1];
+                                        i++;
+                                } while (env_temp[i + 1] != NULL);
+                                env_temp[i] = NULL;
+                        }
+                }
+                i++;
+        }
+        return (0);
 }
