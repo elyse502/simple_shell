b89cb53e6d41d0c518db9b2248fe7913166b60b8
NIYIBIZI Elysée <125453474+elyse502@users.noreply.github.com>
2023-05-24 06:57:20 +0200

    Strings2.c

--- _strings2.c
+++ _strings2.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * _putchar - Writes the character c to stdout.
  * @c: The character to print.
@@ -8,7 +7,7 @@
  */
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
 /**
@@ -20,35 +19,35 @@ int _putchar(char c)
 
 int _strcmp(char *s1, char *s2)
 {
-        int i = 0;
+	int i = 0;
 
-        while (s1[i] != '\0' || s2[i] != '\0')
-        {
-                if (s1[i] != s2[i])
-                        return (s1[i] - s2[i]);
-                i++;
-        }
-        return (0);
+	while (s1[i] != '\0' || s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
 }
 /**
  * c_atoi - Custom atoi converts string to int.
- * @s: String.
+ * @s: string.
  * Return: Number if success, -1 if string contains non-numbers.
  */
 int c_atoi(char *s)
 {
-        int i = 0;
-        unsigned int num = 0;
+	int i = 0;
+	unsigned int num = 0;
 
-        while (s[i] != '\0')
-        {
-                if (s[i] >= '0' && s[i] <= '9') /* calculate num */
-                        num = num * 10 + (s[i] - '0');
-                if (s[i] > '9' || s[i] < '0') /* account for non-numbers */
-                        return (-1);
-                i++;
-        }
-        return (num);
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9') /* calculate num */
+			num = num * 10 + (s[i] - '0');
+		if (s[i] > '9' || s[i] < '0') /* account for non-numbers */
+			return (-1);
+		i++;
+	}
+	return (num);
 }
 
 
@@ -58,18 +57,18 @@ int c_atoi(char *s)
  * to the buffer pointed to by dest.
  * @dest: Copy source to this buffer.
  * @src: This is the source to copy.
- * Return: copy of original source.
+ * Return: Copy of original source.
  */
 
 char *_strcpy(char *dest, char *src)
 {
-        int i, len;
+	int i, len;
 
-        for (len = 0; src[len] != '\0'; len++)
-                ;
+	for (len = 0; src[len] != '\0'; len++)
+		;
 
-        for (i = 0; i <= len; i++)
-                dest[i] = src[i];
+	for (i = 0; i <= len; i++)
+		dest[i] = src[i];
 
-        return (dest);
+	return (dest);
 }
